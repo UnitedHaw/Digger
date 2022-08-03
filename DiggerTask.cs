4b05fb31bf531664bdcee2a4aac0cac2938fa14d
HAwka <v-petrischev2010@yandex.ru>
2022-08-04 01:14:08 +0300

    act method

--- DiggerTask.cs
+++ DiggerTask.cs
@@ -8,5 +8,64 @@ namespace Digger
 {
     //Напишите здесь классы Player, Terrain и другие.
 
-   
+    public class Player : ICreature
+    {
+        public CreatureCommand Act(int x, int y)
+        {
+            CreatureCommand creatureCommand = new CreatureCommand();
+
+            if (x < Game.MapWidth && y < Game.MapHeight)
+            {
+                creatureCommand.DeltaX = x;
+                creatureCommand.DeltaY = y;
+                creatureCommand.TransformTo = null;
+
+                return creatureCommand;
+            }
+            else
+            {
+                return new CreatureCommand { };
+            }
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            string conflictedObjectName = conflictedObject.GetImageFileName();
+            return !conflictedObjectName.Equals("Monster.png");
+        }
+
+        public int GetDrawingPriority()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetImageFileName()
+        {
+            return "Digger.png";
+        }
+    }
+
+    public class Terrain : ICreature
+    {
+        public CreatureCommand Act(int x, int y)
+        {
+            return new CreatureCommand { DeltaX = x - Game.MapWidth, DeltaY = y - Game.MapHeight, TransformTo = null };
+            
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            return false;
+        }
+
+        public int GetDrawingPriority()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetImageFileName()
+        {
+            return "Terrain.png";
+        }
+    }
 }
